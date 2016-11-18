7040fe8d127dc65ac809eeceb59a176328b68a42
John Kim <joimkim@gmail.com>
2016-11-18 10:52:03 -0800

    Add skeleton for app, implement cli runner, implement eratosthenes algo for finding all prime numbers up to n

---
+++ bin/eratosthenes.js
@@ -0,0 +1,36 @@
+// Eratosthenes algorithm for finding all primes. 
+// Time complexity is O(n * log(log(n))).
+var eratosthenes = function(n) {
+
+    let arr = [];
+    let upperLimit = Math.sqrt(n);
+    let output = [];
+
+    // Initially mark all nums from 0 to n as prime 
+    for (let i = 0; i <= n; i++) {
+        arr.push(true);
+    }
+
+    // Remove multiples of prime starting from 2
+    for (let i = 2; i <= upperLimit; i++) {
+        if (arr[i]) {
+            // Only run if current i square is less than n
+            for (let j = i * i; j < n; j += i) {
+                arr[j] = false;
+            }
+        }
+    }
+
+    // Push remaining items in array to output
+    for (let i = 2; i <= n; i++) {
+        if(arr[i]) {
+            output.push(i);
+        }
+    }
+    return output;
+};
+
+module.exports = {
+    getAllPrimes: eratosthenes
+} 
+
---
+++ run.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+
+const eratosthenes = require('./bin/eratosthenes.js');
+
